bc34310342d8e6a751336530a835c299bf2c9f3f
David Hutchison <dhutchis68516@gmail.com>
2020-05-07 20:58:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,13 @@
 def key_for_min_value(name_hash)
   min = nil
   min_name = nil
-  
+
   name_hash.each do |key, value|
     if min == nil || value < min
-      min = value 
+      min = value
       min_name = key
     end
   end
-  
+
   return min_name
 end
